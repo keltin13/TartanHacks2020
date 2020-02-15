97ece7cc89d874ae20e889a4734caad4f4735265
alexdwhite42 <57324111+alexdwhite42@users.noreply.github.com>
2020-02-15 12:44:13 -0500

    plots woohoo

--- Website/app/userstats/running_total.py
+++ Website/app/userstats/running_total.py
@@ -72,9 +72,33 @@ def visualize(bet_history, user_id):
     plt.ylabel("WINNINGS")
     plt.title(f"{username} WINNINGS")
     plt.show()
+    return plt
+
+'''
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
 
 
 running_total("000001")
 visualize(running_total("000001"), "000001")
+'''
 
 
